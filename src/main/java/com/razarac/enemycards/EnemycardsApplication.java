16193f3666060ba3bd4dfad7d9348ac2f5ef3967
John Orji <john.orji@cognizant.com>
2021-03-05 11:47:57 -0600

    initial setup complete

---
+++ src/main/java/com/razarac/enemycards/EnemycardsApplication.java
@@ -0,0 +1,13 @@
+package com.razarac.enemycards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemycardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemycardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/razarac/enemycards/EnemycardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razarac.enemycards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnemycardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
