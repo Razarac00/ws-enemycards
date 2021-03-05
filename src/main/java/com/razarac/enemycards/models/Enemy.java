fb293c1671e2cb5e1496c28370a2755524ed4852
John Orji <john.orji@cognizant.com>
2021-03-05 15:26:41 -0600

    added annotations using javax

--- src/main/java/com/razarac/enemycards/models/Enemy.java
+++ src/main/java/com/razarac/enemycards/models/Enemy.java
@@ -1,27 +1,37 @@
 package com.razarac.enemycards.models;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.List;
+
+import javax.validation.constraints.*;
 
 import com.razarac.enemycards.models.enums.EnemyElement;
 
-@Getter @Setter @NoArgsConstructor
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Enemy {
 
+    @NotNull("Enemy id is required")
+    private @Setter(AccessLevel.PROTECTED) Long id;
+
+    @NotNull("Enemy name is required")
     private String name;
+    @NotNull("Enemy weaknesses are required")
     private List<EnemyElement> weaknesses;
+    @NotNull("Enemy resistances are required")
     private List<EnemyElement> resistances;
+    
     private String image;
     private String description;
 
-    public Enemy(String name, List<EnemyElement> weaknesses, List<EnemyElement> resistances, String image, String description) {
+    public Enemy(Long id, String name, List<EnemyElement> weaknesses, List<EnemyElement> resistances, String image, String description) {
+        this.id = id;
         this.name = name;
         this.image = image;
         this.description = description;
-        
+
         this.weaknesses = weaknesses;
         this.resistances = resistances;
     }
