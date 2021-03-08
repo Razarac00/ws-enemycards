2e0b54b0f5a5f20906999b25026af3e67c055323
John Orji <john.orji@cognizant.com>
2021-03-08 16:41:31 -0600

    added openfeign for client, updated application yaml

--- src/main/java/com/razarac/enemycards/controllers/clients/EnemyCardsServiceClient.java
+++ src/main/java/com/razarac/enemycards/controllers/clients/EnemyCardsServiceClient.java
@@ -3,10 +3,26 @@ package com.razarac.enemycards.controllers.clients;
 import com.razarac.enemycards.models.Enemy;
 import com.razarac.enemycards.models.PageModel;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "enemyCardsCRUD", url = "${enemyCardsCRUD.url}")
 public interface EnemyCardsServiceClient {
+    
+    @RequestMapping(
+        method = RequestMethod.GET,
+        value = "/enemies/{id}"
+    )
+    Enemy getEnemy(@PathVariable("id") Long id);
 
-    Enemy getEnemy(Long id);
-
-    PageModel getEnemies(Integer pageNumber, Integer pageSize, String search);
+    @RequestMapping(
+        method = RequestMethod.GET,
+        value = "/enemies?pageNumber={pageNumber}&pageSize={pageSize}&search={search}"
+    )
+    PageModel getEnemies(
+                @RequestParam("pageNumber") Integer pageNumber, 
+                @RequestParam("pageSize") Integer pageSize, 
+                @RequestParam(value = "search", required = false) String search
+                );
     
 }
