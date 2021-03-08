fe3fb4dde3e667215e718133e5aa694cc4747f44
John Orji <john.orji@cognizant.com>
2021-03-08 11:36:43 -0600

    added pagination model

---
+++ src/main/java/com/razarac/enemycards/models/PageModel.java
@@ -0,0 +1,35 @@
+package com.razarac.enemycards.models;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PageModel {
+
+    private List<Enemy> content;
+
+    private Integer pageTotal, pageSize, pageNumber;
+
+    private Integer enemyTotal, enemyOffset;
+
+    private Boolean hasNext, hasPrevious;
+
+    public PageModel(List<Enemy> content, 
+                    Integer pageTotal, Integer pageSize, Integer pageNumber, 
+                    Integer enemyTotal, Integer enemyOffset, 
+                    Boolean hasNext, Boolean hasPrevious) {
+        this.content = content;
+        
+        this.pageTotal = pageTotal;
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+
+        this.enemyTotal = enemyTotal;
+        this.enemyOffset = enemyOffset;
+
+        this.hasNext = hasNext;
+        this.hasPrevious = hasPrevious;
+    }
+}
